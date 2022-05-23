a953d3b0ba54f1067396d3af725aecbda5fe342f
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-05-23 11:22:34 -0300

    build: add urbanist font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["'Urbanist'", "'Inter'", "system-ui", "sans-serif"],
+    },
     extend: {},
   },
+  daisyui: {
+    themes: ["winter", "night"],
+  },
   plugins: [require("daisyui")],
-}
+};
