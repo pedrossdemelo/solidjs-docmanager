fbd9598bca2d5cce6c8f76a903f5bf72a12ac591
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-05-26 14:07:40 -0300

    feat: add `formatDate` util

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,8 @@
+export default (date: string) =>
+  new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  }).format(new Date(date));
