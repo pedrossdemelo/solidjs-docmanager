4bfd3a89c27082871794434de4f2acf5ce6a3b3b
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-05-26 14:13:20 -0300

    feat: add `User` page

---
+++ src/pages/User.tsx
@@ -0,0 +1,36 @@
+import { User as TUser } from "@types";
+import { UserCard } from "components";
+import { useParams } from "solid-app-router";
+import { createResource, ErrorBoundary, onCleanup, Show } from "solid-js";
+import { reqDelegate } from "utils";
+
+export default function User() {
+  const params = useParams<{ email: string }>();
+
+  const [user, { refetch  }] = createResource<TUser, string>(
+    () => params.email,
+    email => reqDelegate.Users.get(email)
+  );
+
+  const refetcher = setInterval(() => {
+    document.hasFocus() && refetch();
+  }, 60000);
+
+  onCleanup(() => {
+    clearInterval(refetcher);
+  });
+
+  return (
+    <>
+      <ErrorBoundary fallback="Not found">
+        <Show when={!!user() && !user.loading} fallback="Loading...">
+          <UserCard {...user()!} />
+        </Show>
+      </ErrorBoundary>
+
+      <button class="btn btn-info fixed bottom-5 right-5" onClick={refetch}>
+        Refetch
+      </button>
+    </>
+  );
+}
