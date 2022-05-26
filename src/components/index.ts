641cc4f9c2071b8e6130ae8f7df1351eb32e1fa8
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-05-26 14:10:26 -0300

    chore: index components foler

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as CompanyCard } from "./CompanyCard";
+export { default as DocumentCard } from "./DocumentCard";
+export { default as Hero } from "./Hero";
+export { default as Nav } from "./Nav";
+export { default as UserCard } from "./UserCard";
