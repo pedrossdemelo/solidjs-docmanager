871293a0ce5ef256b63ae4e32b4c982ffd55b806
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-05-26 13:26:32 -0300

    feat: add providers to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,17 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
-
 import "./index.css";
 import App from "./App";
+import { AuthProvider } from "context";
+import { Router } from "solid-app-router";
 
-render(() => <App />, document.body);
+render(
+  () => (
+    <Router>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Router>
+  ),
+  document.body
+);
