7ae732baa8ac4489c134cf4480d9bff227a5b57a
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-05-26 14:09:55 -0300

    feat: add `Nav` component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,21 +1,37 @@
-import { useMatch } from "solid-app-router";
+import { useAuth } from "context";
+import { useMatch, Link } from "solid-app-router";
 import { Show } from "solid-js";
 
 export default (props: any) => {
   const isHome = useMatch(() => "/");
+  const [user, { logout }] = useAuth();
   return (
     <nav class="navbar bg-base-100 w-auto rounded-lg m-2 shadow-lg z-99 overflow-visible">
       <div class="navbar-start">
         <DropdownMenu />
-        <a class="btn btn-ghost normal-case text-xl">docManager</a>
+        <Link href="/" class="btn btn-ghost normal-case text-xl">
+          docManager
+        </Link>
       </div>
 
       <CenterMenu />
 
       <div class="navbar-end gap-2">
-        <Show when={!isHome()} fallback={<GitHubButton />}>
-          <a class="btn btn-outline btn-primary">Login</a>
-          <a class="btn btn-primary">Sign up</a>
+        <Show when={!isHome() && !user()} fallback={!user() && <GitHubButton />}>
+          <Link href="/" class="btn btn-outline btn-primary">
+            Login
+          </Link>
+          <Link href="/" class="btn btn-primary">
+            Sign up
+          </Link>
+        </Show>
+        <Show when={!!user()}>
+          <Link href={`/users/${user()?.email}`} class="btn btn-ghost">
+            Profile
+          </Link>
+          <Link onClick={logout} href="/" class="btn btn-outline">
+            Logout
+          </Link>
         </Show>
       </div>
     </nav>
@@ -25,13 +41,13 @@ export default (props: any) => {
 const PageLinks = () => (
   <>
     <li>
-      <a>Community</a>
+      <Link href="/docs">Documents</Link>
     </li>
     <li>
-      <a>Documents</a>
+      <Link href="/companies">Companies</Link>
     </li>
     <li>
-      <a>Companies</a>
+      <Link href="/users">Community</Link>
     </li>
   </>
 );
@@ -46,12 +62,7 @@ const DropdownMenu = () => (
         viewBox="0 0 24 24"
         stroke="currentColor"
       >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M4 6h16M4 12h8m-8 6h16"
-        />
+<Path     />
       </svg>
     </label>
     <ul
@@ -80,3 +91,8 @@ const GitHubButton = () => (
     Github
   </a>
 );
+
+    function Path({}) {
+      return (<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />);
+    }
+  
